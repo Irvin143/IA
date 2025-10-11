e3e7b1d1807ff43edab1c259af928a6d6fcba523
Irvin <L22170596@culiacan.tecnm.mx>
2025-10-10 20:09:18 -0700

    Integracion los botones de ingredientes

--- Unidad2/SistemaRecomendacion/Codigo/Frame.py
+++ Unidad2/SistemaRecomendacion/Codigo/Frame.py
@@ -6,7 +6,6 @@ class FrameIngredientes(ctk.CTkFrame):
         self.selected = set()
         self.buttons = {}
         self.mode = mode
-
         
         ctk.CTkLabel(self, text=mode, font=("Arial", 18, "bold")).pack(anchor="w", pady=(10, 5)) 
         btn_frame = ctk.CTkFrame(self)
@@ -30,24 +29,6 @@ class FrameIngredientes(ctk.CTkFrame):
                     command=lambda s=sabor: self.toggle(s))
             btn.grid(row=i // cols, column=i % cols, padx=5, pady=5, sticky="w")
             self.buttons[sabor] = btn
-        
-        
-
-        """ctk.CTkLabel(btn_frame, text="Restricciones", font=("Arial", 18, "bold")).pack(anchor="w", pady=(10, 5)) 
-        scroll2 = ctk.CTkScrollableFrame(btn_frame, height=140, width=1500)
-        scroll2.pack(anchor="w", fill="x", expand=True)
-        cols = 9  # ajustar número de columnas por fila según convenga
-
-        for i, sabor in enumerate(category):
-            saborRestriccion = sabor + "(No)"
-            btn = ctk.CTkButton(scroll2, text=sabor,
-                    fg_color=normal_color,
-                    font=("Arial", 14),
-                    command=lambda s=saborRestriccion: self.toggle(s, mode="restricciones"))
-            btn.grid(row=i // cols, column=i % cols, padx=5, pady=5, sticky="w")
-            self.buttons[saborRestriccion] = btn"""
-
-        
 
     def toggle(self, opt):
         if self.mode == "Gustos":
